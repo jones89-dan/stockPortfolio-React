14614a0da8b0564b314c4bb5cb3b1b5439113e28
Daniel Jones <danielj@mythostech.com>
2021-11-18 14:47:16 -0800

    created index.htm and script.js with basic framework

---
+++ src/script.js
@@ -0,0 +1,37 @@
+class Portfolio extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center my-4">Stock Portfolio</h1>
+        <div className="row">
+          <div className="col-12">
+            <table className="table table-responsive">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Shares Owned</th>
+                  <th scope="col">Cost per share ($)</th>
+                  <th scope="col">Market Price ($)</th>
+                  <th scope="col">Market Value ($)</th>
+                  <th scope="col">Unrealized Gain/Loss ($)</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Portfolio />,
+  document.getElementById('root')
+);
