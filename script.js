a478d4b5dbf3f0304569a1d522a724bfaaea05ef
jones89-dan <jones.89.dan@gmail.com>
2021-11-19 11:41:03 -0800

    Added code to render stocks in tbody, compiled react code

--- script.js
+++ script.js
@@ -12,71 +12,136 @@ var Portfolio = function (_React$Component) {
   function Portfolio(props) {
     _classCallCheck(this, Portfolio);
 
-    return _possibleConstructorReturn(this, (Portfolio.__proto__ || Object.getPrototypeOf(Portfolio)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Portfolio.__proto__ || Object.getPrototypeOf(Portfolio)).call(this, props));
+
+    _this.state = {
+      portfolio: [{
+        name: 'Feetbook',
+        shares_owned: 20,
+        cost_per_share: 50,
+        market_price: 130
+      }, {
+        name: 'Yamazon',
+        shares_owned: 5,
+        cost_per_share: 200,
+        market_price: 500
+      }, {
+        name: 'Snoozechat',
+        shares_owned: 100,
+        cost_per_share: 20,
+        market_price: 3
+      }]
+    };
+    return _this;
   }
   //Render method
 
 
   _createClass(Portfolio, [{
-    key: "render",
+    key: 'render',
     value: function render() {
       return React.createElement(
-        "div",
-        { className: "container" },
+        'div',
+        { className: 'container' },
         React.createElement(
-          "h1",
-          { className: "text-center my-4" },
-          "Stock Portfolio"
+          'h1',
+          { className: 'text-center my-4' },
+          'Stock Portfolio'
         ),
         React.createElement(
-          "div",
-          { className: "row" },
+          'div',
+          { className: 'row' },
           React.createElement(
-            "div",
-            { className: "col-12" },
+            'div',
+            { className: 'col-12' },
             React.createElement(
-              "table",
-              { className: "table table-responsive" },
+              'table',
+              { className: 'table table-responsive' },
               React.createElement(
-                "thead",
+                'thead',
                 null,
                 React.createElement(
-                  "tr",
+                  'tr',
                   null,
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Name"
+                    'th',
+                    { scope: 'col' },
+                    'Name'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Shares Owned"
+                    'th',
+                    { scope: 'col' },
+                    'Shares Owned'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Cost per share ($)"
+                    'th',
+                    { scope: 'col' },
+                    'Cost per share ($)'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Market Price ($)"
+                    'th',
+                    { scope: 'col' },
+                    'Market Price ($)'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Market Value ($)"
+                    'th',
+                    { scope: 'col' },
+                    'Market Value ($)'
                   ),
                   React.createElement(
-                    "th",
-                    { scope: "col" },
-                    "Unrealized Gain/Loss ($)"
+                    'th',
+                    { scope: 'col' },
+                    'Unrealized Gain/Loss ($)'
                   ),
-                  React.createElement("th", { scope: "col" })
+                  React.createElement('th', { scope: 'col' })
                 )
               ),
-              React.createElement("tbody", null)
+              React.createElement(
+                'tbody',
+                null,
+                portfolio.map(function (stock, index) {
+                  var name = stock.name,
+                      shares_owned = stock.shares_owned,
+                      cost_per_share = stock.cost_per_share,
+                      market_price = stock.market_price;
+
+                  return React.createElement(
+                    'tr',
+                    { key: index },
+                    React.createElement(
+                      'td',
+                      null,
+                      name
+                    ),
+                    React.createElement(
+                      'td',
+                      null,
+                      React.createElement('input', { type: 'number', name: 'shares_owned', value: shares_owned })
+                    ),
+                    React.createElement(
+                      'td',
+                      null,
+                      React.createElement('input', { type: 'number', name: 'cost_per_share', value: cost_per_share })
+                    ),
+                    React.createElement(
+                      'td',
+                      null,
+                      React.createElement('input', { type: 'number', name: 'market_price', value: market_price })
+                    ),
+                    React.createElement('td', null),
+                    React.createElement('td', null),
+                    React.createElement(
+                      'td',
+                      null,
+                      React.createElement(
+                        'button',
+                        { className: 'btn btn-light btn-sm' },
+                        'remove'
+                      )
+                    )
+                  );
+                })
+              )
             )
           )
         )
--- src/script.js
+++ src/script.js
@@ -22,7 +22,7 @@ class Portfolio extends React.Component {
         }
       ]
     };
-  }
+}
 //Render method
   render() {
     return (
@@ -43,6 +43,25 @@ class Portfolio extends React.Component {
                 </tr>
               </thead>
               <tbody>
+              {portfolio.map((stock, index) => {
+                const {
+                  name,
+                  shares_owned,
+                  cost_per_share,
+                  market_price,
+                } = stock;
+                return (
+                  <tr key={index}>
+                    <td>{name}</td>
+                    <td><input type="number" name="shares_owned" value={shares_owned} /></td>
+                    <td><input type="number" name="cost_per_share" value={cost_per_share} /></td>
+                    <td><input type="number" name="market_price" value={market_price} /></td>
+                    <td></td>
+                    <td></td>
+                    <td><button className="btn btn-light btn-sm">remove</button></td>
+                  </tr>
+                )
+              })}
               </tbody>
             </table>
           </div>
